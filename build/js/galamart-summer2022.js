1cdc091b7d351b6332d5b3983e39c35839150818
Maxim Karachyov <karachyov.m@gmail.com>
2022-04-26 23:18:26 +0300

    project

---
+++ build/js/galamart-summer2022.js
@@ -0,0 +1,39 @@
+const swiperSummer2022 = new Swiper('.swiper-summer2022', {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+        1300: {
+            slidesPerView: 4,
+        }
+
+
+    }
+});
+
+const swiperSummer2022Poster = new Swiper('.swiper-summer2022-poster', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
